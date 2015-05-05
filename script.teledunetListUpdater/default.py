3d54b52d9e4ea53f7bdf50d782388566384e9bda
pampereo <aymsai@hotmail.com>
2015-05-05 11:31:45 +0200

    Teldunet Updater v1.0.4

--- script.teledunetListUpdater/default.py
+++ script.teledunetListUpdater/default.py
@@ -10,7 +10,8 @@ addon_id = 'script.teledunetListUpdater'
 
 __addon__ = xbmcaddon.Addon(addon_id)
 df =  __addon__.getSetting("folder")
-clr = __addon__.getSetting("delsetting") == 'true'
+clr = __addon__.getSetting("dellogin") == 'true'
+cln = __addon__.getSetting("delsetting") == 'true'
 AddName = __addon__.getAddonInfo('name') 
 
 values = {'login_user' : __addon__.getSetting("username"),
@@ -32,12 +33,21 @@ pEXTM3U = True
 
 class Main():
           
-    def cleanLogin(self):
+    def clearLogin(self):
         __addon__.setSetting('username','')
         __addon__.setSetting('password','')
         __addon__.setSetting('folder','')
+        __addon__.setSetting('dellogin','false')
+        
+    def cleanSetting(self):
+        __addon__.setSetting('colSrv1','yellow')
+        __addon__.setSetting('colSrv2','blue')
+        __addon__.setSetting('srv1Name','Srv1')
+        __addon__.setSetting('srv2Name','Srv2')
+        __addon__.setSetting('paramKodi',' live=true')
+        __addon__.setSetting('oname','teledunetKodi.m3u')
         __addon__.setSetting('delsetting','false')
-
+     
     def processFile(self,inData,outData,wlabel,gr):
         
         global pEXTM3U
@@ -60,12 +70,21 @@ class Main():
                 outData.write(x+"\n") 
     
     def go(self):
-        
-        if clr :
-            self.cleanLogin()
-            xbmcgui.Dialog().ok(AddName,"Setting deleted !!")
+               
+        if clr and cln :
+            self.clearLogin()
+            self.cleanSetting()
+            xbmcgui.Dialog().ok(AddName,"Login and Settings deleted !!")
             exit()
-         
+        elif not clr and cln :
+            self.cleanSetting()
+            xbmcgui.Dialog().ok(AddName,"Settings deleted !!")
+            exit()
+        elif clr and not cln:
+            self.clearLogin()
+            xbmcgui.Dialog().ok(AddName,"Login deleted !!")
+            exit()
+            
         data = urllib.urlencode(values)
         cookies = cookielib.CookieJar()
         opener = urllib2.build_opener(
@@ -82,7 +101,7 @@ class Main():
             data1 = opener.open(down_url1).readlines()
             data2 = opener.open(down_url2).readlines() 
             out = open(Oname,"w")                
-              
+            
             self.processFile(data1, out, label1, groupe1) 
             self.processFile(data2, out, label2, groupe2)
             
